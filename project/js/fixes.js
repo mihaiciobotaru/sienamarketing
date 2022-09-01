1ed2846b25a4d0e443cdd719529e842ada68a126
Ciobotaru Mihai <mihai2096@gmail.com>
2022-09-01 17:59:35 +0300

    uploaded siena marketing

---
+++ project/js/fixes.js
@@ -0,0 +1,5 @@
+jQuery('video').on("loadeddata", function() {
+    jQuery('video').attr('controlsList', 'nodownload');
+    jQuery('video').bind('contextmenu',function() { return false; });
+    jQuery('video').attr('disablePictureInPicture', 'true');
+}); 
